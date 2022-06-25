0c206c5f672f2abbaeee63f94070a9d84285a2c0
simskiy <simskiy@gmail.com>
2022-06-25 19:13:25 +0300

    Вынос scss-миксинов, scss-переменных и подключение шрифотов в отдельные файлы

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,15 @@ const compilePug = require('./gulp/tasks/compilePug')
 const compileSass = require('./gulp/tasks/compileSass')
 const clean = require('./gulp/tasks/clean')
 const serve = require('./gulp/tasks/serve')
+const copyImg = require('./gulp/tasks/copyImg')
+const copyFonts = require('./gulp/tasks/copyFonts')
 
 exports.build = series(
   clean,
-  parallel(compilePug, compileSass)
+  parallel(compilePug, compileSass, copyFonts, copyImg)
 )
 
 exports.dev = series(
-  parallel(compileSass, compilePug),
+  parallel(compileSass, compilePug, copyFonts, copyImg),
   serve
 )
